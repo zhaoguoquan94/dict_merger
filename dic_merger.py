a303de21b43a322b84969afb765a8e5e399c83c0
Zhaoguoquan <zhaoguoquan94@gmail.com>
2014-02-28 20:33:53 +0800

    nothing inside

---
+++ dic_merger.py
@@ -0,0 +1,27 @@
+import os
+import math
+import json
+
+
+if __name__ == '__main__':
+	inputFileName=input("input file name")
+	outputFileName=input("outputFileName")
+
+
+
+	dic_old=set()
+	dic_new=set()
+
+
+	#dic merge
+
+
+	#dic old input
+
+	#dic new input 
+
+	#update dic_new
+
+	#write dic_new back to file
+
+	
