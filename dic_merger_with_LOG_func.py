3da540da678928dbb2dcbc151c272294d76d7323
Zhaoguoquan <zhaoguoquan94@gmail.com>
2014-03-01 10:50:03 +0800

    dic_merger version3 usage: in command line type python file1 file2 file3 file4 file1:user's json file file2:server's json file file3:merged json file file4:log file,indicating which item has been added

--- dic_merger_with_LOG_func.py
+++ dic_merger_with_LOG_func.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+import time
 def main():
 	# jsonFile1=input("input json file 1's path")
 	# jsonFile2=input("input json file 2's path")
@@ -8,16 +9,25 @@ def main():
 	jsonFile1=sys.argv[1]
 	jsonFile2=sys.argv[2]
 	outputFileName=sys.argv[3]
+	logFileName=sys.argv[4]
 	f1=open(jsonFile1)
 	f2=open(jsonFile2)
 	fout=open(outputFileName,"w")
+	fLog=open(logFileName,"w")
 	dic1=json.loads(f1.read())
 	dic2=json.loads(f2.read())
 	dicOut=dict(dic1,**dic2)
+	fLog.write("add these items \n"+str(time.asctime())+"\n")
+	fLog.write("	key{0:26}".format("")+"value"+"\n\n\n")
+	for key in dic2:
+		if key not in dic1:
+			fLog.write("	{0:30}".format(key)+str(dic2[key])+"\n")
+			# fLog.write(		key+"+++"+str(dic1[key])+"\n")
 	json.dump(dicOut,fout)
 	f1.close()
 	f2.close()
 	fout.close()
+	fLog.close()
 
 	
 if __name__ == '__main__':
