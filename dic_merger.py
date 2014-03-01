a7c235b21ec9d03cd36deafe3ae07c318ec13e64
Zhaoguoquan <zhaoguoquan94@gmail.com>
2014-03-01 08:48:34 +0800

    dic_merger version1 is able to merge two json file need further improment

--- dic_merger.py
+++ dic_merger.py
@@ -1,27 +1,21 @@
-import os
-import math
 import json
 
+def main():
+	jsonFile1=input("input json file 1's path")
+	jsonFile2=input("input json file 2's path")
+	outputFileName=input("outputFileName's path")
+	f1=open(jsonFile1)
+	f2=open(jsonFile2)
+	fout=open(outputFileName,"w")
+	dic1=json.loads(f1.read())
+	dic2=json.loads(f2.read())
+	dicOut=dict(dic1,**dic2)
+	json.dump(dicOut,fout)
+	f1.close()
+	f2.close()
+	f2.close()
 
+	
 if __name__ == '__main__':
-	inputFileName=input("input file name")
-	outputFileName=input("outputFileName")
-
-
-
-	dic_old=set()
-	dic_new=set()
-
-
-	#dic merge
-
-
-	#dic old input
-
-	#dic new input 
-
-	#update dic_new
-
-	#write dic_new back to file
-
+	main()
 	
