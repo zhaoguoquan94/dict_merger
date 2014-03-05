8955cb93f71a576011c17a2091891945b7d56e9a
Zhaoguoquan <zhaoguoquan94@gmail.com>
2014-03-05 21:58:40 +0800

    all finished .  Wait for test Maybe bugs still exists.

--- test_case/testSingleMerge.py
+++ test_case/testSingleMerge.py
@@ -1,11 +1,12 @@
+###Useage:  in command line
+#python3 testSingleMerge [userJsonFile] [serverJsonFile] [outputFile]
+
 import json
 import os
 import sys
 import time
 def main():
-	# jsonFile1=input("input json file 1's path")
-	# jsonFile2=input("input json file 2's path")
-	# outputFileName=input("outputFileName's path")
+
 	jsonFile1=sys.argv[1]
 	jsonFile2=sys.argv[2]
 	outputFileName=sys.argv[3]
